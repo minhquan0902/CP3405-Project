b620899aa8388f0a28e82cfdc95c595cc4f9027a
minhquan0902 <minhquan0902@gmail.com>
2021-04-09 03:27:23 +0800

    Mobile Phone OTP Login Feature

--- app/src/main/java/au/edu/jcu/cp3406/designthinkingiii/MainActivity.java
+++ app/src/main/java/au/edu/jcu/cp3406/designthinkingiii/MainActivity.java
@@ -2,13 +2,46 @@ package au.edu.jcu.cp3406.designthinkingiii;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mLogOutBtn;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button mLogOutBtn = findViewById(R.id.log_out_btn);
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
+        mLogOutBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                finish();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser == null) {
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            finish();
+        }
     }
 }
